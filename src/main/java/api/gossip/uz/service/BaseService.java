642a57c13d41307cc9efc098e03bff04e4f601b8
Otabek Abdulazizov <oabdulazizov@cdti.uz>
2025-02-16 11:37:48 +0500

    and notFound

--- src/main/java/api/gossip/uz/service/BaseService.java
+++ src/main/java/api/gossip/uz/service/BaseService.java
@@ -1,7 +1,7 @@
 package api.gossip.uz.service;
 
+import api.gossip.uz.exception.BadRequestException;
 import api.gossip.uz.exception.EntityNotFound;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -20,5 +20,33 @@ public class BaseService {
 
     protected Supplier<EntityNotFound> notFound(final String entity) {
         return () -> new EntityNotFound(resourceBundleService.getMessage("not.found", entity));
+    }
+
+    protected Supplier<EntityNotFound> notFound() {
+        return () -> new EntityNotFound(resourceBundleService.getMessage(""));
+    }
+
+    protected Supplier<EntityNotFound> notFound(final String entity, final Integer errorCode) {
+        return () -> new EntityNotFound(resourceBundleService.getMessage("not.found", entity), errorCode);
+    }
+
+    protected Supplier<EntityNotFound> notFound(final String entity, final Long id) {
+        return () -> new EntityNotFound((resourceBundleService.getMessage("not.found", entity, id)));
+    }
+
+    protected Supplier<EntityNotFound> notFound(final String entity, final String id) {
+        return () -> new EntityNotFound(resourceBundleService.getMessage("not.found", entity, id));
+    }
+
+    protected Supplier<EntityNotFound> notFound(final String entity, final String id, final Integer errorCode) {
+        return () -> new EntityNotFound(resourceBundleService.getMessage("not.found", entity, id), errorCode);
+    }
+
+    protected Supplier<BadRequestException> badRequest(String message) {
+        return () -> new BadRequestException(message);
+    }
+
+    protected Supplier<BadRequestException> badRequest(String message, int errorCode) {
+        return () -> new BadRequestException(message, errorCode);
     }
 }
