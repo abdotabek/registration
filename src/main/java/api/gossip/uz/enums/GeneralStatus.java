5bf305375fc3f18ce64542703579dd94a6ecd3f3
Otabek Abdulazizov <oabdulazizov@cdti.uz>
2025-05-03 09:22:19 +0500

    added index

--- src/main/java/api/gossip/uz/enums/GeneralStatus.java
+++ src/main/java/api/gossip/uz/enums/GeneralStatus.java
@@ -1,6 +1,14 @@
 package api.gossip.uz.enums;
 
-public enum GeneralStatus {
-    ACTIVE, BLOCK, IN_REGISTRATION, NOT_ACTIVE
+import lombok.Getter;
 
+@Getter
+public enum GeneralStatus {
+    ACTIVE(1), BLOCK(2),
+    IN_REGISTRATION(3), NOT_ACTIVE(4);
+    private final int numberOfGeneraStatus;
+
+    GeneralStatus(int size) {
+        this.numberOfGeneraStatus = size;
+    }
 }
